7de354f28085319c14b3d65523d48a20c92822c7
minhnhan210196 <nhanpm@selex.vn>
2023-08-01 09:36:00 +0700

    creat project

---
+++ Src/main.c
@@ -0,0 +1,13 @@
+/*
+ * main.c
+ *
+ *  Created on: Aug 1, 2023
+ *      Author: Admin
+ */
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+	printf("Selex Battery Module Manager\n");
+
+	return 0;
+}
